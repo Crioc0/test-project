9b1b404a0a7a2c05d6eefb8df10b347f3a53b013
Denis Perekhodko <pirrekd@mail.ru>
2025-05-17 11:03:45 +0300

    fix user/[user] page

--- src/app/users/[user]/page.tsx
+++ src/app/users/[user]/page.tsx
@@ -4,13 +4,14 @@ import Profile from "@/components/Profile";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-async function UserPage({ params }: { params: { user: string } }) {
+async function UserPage({ params }: { params: Promise<{ user: string }> }) {
+  const { user } = await params;
   try {
-    const data = await getUser(params.user);
+    const data = await getUser(user);
 
     return (
       <>
-        <Link href="/users" className="block mb-4 text-3xl bold">
+        <Link href="/users" className="block mb-4 text-lg bold">
           Назад
         </Link>
         <Profile user={data} />
