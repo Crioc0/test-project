74423d1a93094e89574b843814fe0643fb7baa6e
Denis Perekhodko <pirrekd@mail.ru>
2025-05-17 11:19:38 +0300

    fix api

--- src/api/user.tsx
+++ src/api/user.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 
 const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
-  timeout: 10,
+  timeout: 3000,
   headers: {
     "Content-Type": "application/json",
   },
